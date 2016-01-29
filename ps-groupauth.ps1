cd5ba6251480fab340874e708ff06d070107d6e7
Trevor Bryant <trevorbryant@gmail.com>
2016-01-29 01:01:46 -0500

    Updated to fix vars

--- ps-groupauth.ps1
+++ ps-groupauth.ps1
@@ -1,14 +1,16 @@
 ﻿$Server = hostname
-$Configuration = ""
 $AuthList = "Administrator", "Trevor", "Test" #GC "C:\temp\authlist"
+$Configuration = ""
 
-If (Get-Command -Name Get-LocalGroupMember -ComputerName $server -Eq $True)
+# If cmdlet exists, do work
+If (Get-Command -Name Get-LocalGroupMember -Eq $True)
      {
-         $Configuration = Get-LocalGroupMember Administrators
+         $Configuration = Get-LocalGroupMember -ComputerName $Server Administrators
      } Else {
          $Configuration = "Module LocalAccount is not installed"
 }
 
+# For each User existing in $AuthList, output Authed/Not Authed
 $Configuration | ForEach { If ($_ -In $AuthList)
     {
         Write-Host "$_ : authorized user"
