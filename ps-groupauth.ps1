0502543c9b0baf4ab9cb15935cb3e527705ca8d9
Trevor Bryant <trevorbryant@gmail.com>
2016-01-29 00:48:42 -0500

    Updated readme

---
+++ ps-groupauth.ps1
@@ -0,0 +1,18 @@
+﻿$Server = hostname
+$Configuration = ""
+$AuthList = "Administrator", "Trevor", "Test" #GC "C:\temp\authlist"
+
+If (Get-Command -Name Get-LocalGroupMember -ComputerName $server -Eq $True)
+     {
+         $Configuration = Get-LocalGroupMember Administrators
+     } Else {
+         $Configuration = "Module LocalAccount is not installed"
+}
+
+$Configuration | ForEach { If ($_ -In $AuthList)
+    {
+        Write-Host "$_ : authorized user"
+    } Else {
+        Write-Host "$_ : not authorized user"
+    }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Windows LocalGroup Audit
+Remotely audit Windows local groups for users
+## To-Do
+- * Add indication for WinNT and Domain users
+- * Add, if Domain user, query AD for all Domain user memberships
+- * Currently PowerShell v5 supported, potentially add ADSI WinNT logic
